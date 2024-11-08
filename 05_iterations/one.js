3c21e1a270fbf5b19734b536df95150fe11ebe2e
ChiragBajaj23 <104206376+ChiragBajaj23@users.noreply.github.com>
2024-11-08 18:10:32 +0000

    for/while basics

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+// for loop
+let array = [1,2,3,4,5,5,6,7,8,9,0];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+    
+}
+
+// while
+
+let index= 22;
+while(index > 0) {
+    console.log(index);
+    index--;
+}
+
+// do while
+let inb = 32;
+do {
+    console.log(inb);// will print this
+    inb++;
+} while (inb < 31);
